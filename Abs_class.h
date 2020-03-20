0de22b059a3e8b9b8208b78d9e70d3ad6cf395f1
ArtemPuzikov <Artdmpuzikov@mail.ru>
2020-03-20 13:42:49 +0300

    add new folder

---
+++ Abs_class.h
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+class Weapon
+{
+public:
+    virtual void Attack()
+    {
+
+    };
+};
+
+class Gun : public Weapon
+{
+    void Attack() override
+    {
+        cout << "BANG" << endl;
+    }
+};
+class SubMachineGun : public Gun
+{
+    void Attack() override
+    {
+        cout << "BANG BANG BANG" << endl;
+    }
+};
+class Knife : public Weapon
+{
+public:
+    void Attack() override
+    {
+    }
+};
+class Player
+{
+public:
+    void UseWeapon(Weapon* weapon)
+    {
+        weapon->Attack();
+    }
+};
---
+++ Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "Abs_class.h"
+#include <vector>
+#include <list>
+using namespace std;
+
+int main()
+{
+    // Gun gun;
+    // SubMachineGun sbmg;
+    // Player player;
+    // Knife knife;
+    // player.UseWeapon(&knife);
+    vector<int> myList = {0,1,2,3,4,5};
+    for (auto var : myList)
+    {
+        cout << var << endl;
+    }
+
+
+
+
+    return 0;
+}
